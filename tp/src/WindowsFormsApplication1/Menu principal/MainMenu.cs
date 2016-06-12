9063140d810db454f5d9d0c2b015c2b20e771ec2
Lautaro <garcia.a.lautaro@gmail.com>
2016-06-12 22:56:47 +0000

    Agrego los archivos del menú principal
    Me los olvidé en el commit anterior

---
+++ tp/src/WindowsFormsApplication1/Menu principal/MainMenu.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1.Menu_principal
+{
+    public partial class MainMenu : Form
+    {
+        Dictionary<int, Form> form_mapping;
+
+        public MainMenu(int role_code)
+        {
+            InitializeComponent();
+            this.initialize_form_mapping();
+            this.fill_list(role_code);
+        }
+
+        private void fill_list(int role_code)
+        {
+            // Seteo que el listBox muestre la info adecuada
+            this.listBox1.DisplayMember = "Value";
+            this.listBox1.ValueMember = "Key";
+
+            SqlConnection connection = DBConnection.getInstance().getConnection();
+            SqlCommand query = new SqlCommand("HARDCOR.funcionalidades_del_rol", connection);
+            query.CommandType = CommandType.StoredProcedure;
+            query.Parameters.Add(new SqlParameter("@cod_rol", role_code));
+
+            connection.Open();
+            SqlDataReader reader = query.ExecuteReader();
+            while (reader.Read())
+                this.listBox1.Items.Add(new KeyValuePair<int, string>(Int32.Parse(reader["cod_fun"].ToString()), reader["descripcion"].ToString()));
+            connection.Close();
+        }
+
+        private void initialize_form_mapping()
+        {
+            this.form_mapping = new Dictionary<int, Form>();
+            this.form_mapping.Add(1, new ABM_Rol.AbmRol());
+            this.form_mapping.Add(2, new ABM_Usuario.AbmUsuario());
+            this.form_mapping.Add(3, new ABM_Rubro.AbmRubro());
+            this.form_mapping.Add(4, new ABM_Visibilidad.AbmVisibilidad());
+            this.form_mapping.Add(5, new Generar_Publicación.Form1());
+            this.form_mapping.Add(6, new ComprarOfertar.Form1());
+            this.form_mapping.Add(7, new Historial_Cliente.Form1());
+            this.form_mapping.Add(8, new Calificar.Form1());
+            this.form_mapping.Add(9, new Facturas.Form1());
+            this.form_mapping.Add(10, new Listado_Estadistico.Form1());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int selected_functionality_code = ((KeyValuePair<int, string>) this.listBox1.SelectedItem).Key;
+            if (!this.form_mapping.ContainsKey(selected_functionality_code))
+                return;
+
+            this.Hide();
+            (this.form_mapping[selected_functionality_code]).Show();
+        }
+    }
+}
